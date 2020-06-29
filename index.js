8da802445940bade209dbcb018f5802651783758
Eryoung-610 <eryoung610@gmail.com>
2020-06-29 11:53:07 -0700

    Added NPM packages for middleware action

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ require('dotenv').config()
 const Express = require('express')
 // Require and set view engine use ejs
 const ejsLayouts = require('express-ejs-layouts');
+
+// Require all middleware for app/authentication
+// Helmet, morgan, passport, custom middlware, express-sessions, sequelize sessions
+const helmet = require('helmet')
+const session = require('express-session')
+const flash = require('flash')
+
 // Set app to use false urlencoding
 // Set app public directory for use
 // Set app ejsLayouts for render
@@ -13,6 +20,8 @@ app.use(Express.urlencoded({ extended: false }));
 app.use(Express.static(__dirname + "/public"));
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
+app.use(require('morgan')('dev'));
+app.use(helmet());
 
 // ROUTES
 app.get('/', (req,res) => {
