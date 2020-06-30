09e84c68bf29084e82cffce6e004c2dd44124dcf
Eryoung-610 <eryoung610@gmail.com>
2020-06-30 10:59:33 -0700

    Fixes to routing

--- controller/auth.js
+++ controller/auth.js
@@ -1,20 +1,17 @@
-// require express
-const express = require('express')
-// import router
-const router = express.Router()
-// import db
+const express = require('express');
+const router = express.Router();
 const db = require('../models');
 // import middleware
 const flash = require('flash')
-// signup get route
-// signup post route
+// TODO: update require below to passport config file path
+const passport;
 
-router.get('/register', (req,res) => {
+// register get route
+router.get('/register', function (req, res) {
     res.render('auth/register');
 })
-
 // register post route
-router.post('/register', (req,res) => {
+router.post('/register', function (req, res) {
     db.user.findOrCreate({
         where: {
             email: req.body.email
@@ -22,28 +19,59 @@ router.post('/register', (req,res) => {
             name: req.body.name,
             password: req.body.password
         }
-    }).then ((user,created) => {
-        if(created) {
-            console.log("User created")
-            res.redirect('/')
+    }).then(function ([user, created]) {
+        // if user was created
+        if (created) {
+            // authenticate user and start authorization process
+            console.log("User created! 🎉");
+            res.redirect("/");
         } else {
-            console.log("email already exists")
-            req.flash('error','Error: email already exists for user. Try again');
-            res.redirect('auth/register');
+            console.log("User email already exists 🚫.");
+            req.flash('error', 'Error: email already exists for user. Try again.');
+            res.redirect('/auth/register');
         }
-    }).catch(err => {
-        console.log(`Error found. \nMessage: ${err.message}. \nPlease review - ${err}`)
-        req.flash('error', err.message)
-        res.redirect('/auth/register')
+    }).catch(function (err) {
+        console.log(`Error found. \nMessage: ${err.message}. \nPlease review - ${err}`);
+        req.flash('error', err.message);
+        res.redirect('/auth/register');
     })
 })
 
+
 // login get route
-router.get('/login', (req,res) => {
-    res.render('auth/login')
+router.get('/login', function (req, res) {
+    res.render('/auth/login');
 })
 
+// login post route
+// TODO: pass next param to function
+router.post('/login', function (req, res, next) {
+    passport.authenticate('local', function (error, user, info) {
+        // if no user authenticated
+        if (!user) {
+            req.flash('error', 'Invalid username or password');
+            // save to our user session no username
+            // redirect our user to try logging in again
+        }
+        if (error) {
+            // TODO: add next param from function
+            return error;
+        }
 
+        req.login(function (user, error) {
+            // if error move to error
+            // if success flash success message
+            // if success save session and redirect user
+        })
+    })
+})
+
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/auth/login',
+    successFlash: 'Welcome to our app!',
+    failureFlash: 'Invalid username or password.'
+}));
 
 // export router
 module.exports = router;
--- index.js
+++ index.js
@@ -1,40 +1,32 @@
-// Configure dotenv
-require('dotenv').config()
-// Require express and setup and express app instance
-const Express = require('express')
-// Require and set view engine use ejs
-const ejsLayouts = require('express-ejs-layouts');
+// Required NPM libraries
+require('dotenv').config();
+const Express = require('express');
+const ejsLayouts = require("express-ejs-layouts");
+// passport, and custom middleware, sequelize sessions
+const helmet = require('helmet');
+const session = require("express-session");
+const flash = require("flash");
 
-// Require all middleware for app/authentication
-// Helmet, morgan, passport, custom middlware, express-sessions, sequelize sessions
-const helmet = require('helmet')
-const session = require('express-session')
-const flash = require('flash')
 
-// Set app to use false urlencoding
-// Set app public directory for use
-// Set app ejsLayouts for render
-
+// app setup
 const app = Express();
 app.use(Express.urlencoded({ extended: false }));
 app.use(Express.static(__dirname + "/public"));
-app.set('view engine', 'ejs');
+app.set('view engine','ejs');
 app.use(ejsLayouts);
 app.use(require('morgan')('dev'));
 app.use(helmet());
 
 // ROUTES
-app.get('/', (req,res) => {
-    // Check to see if user logged in
-    res.render('index')
+app.get('/', function(req, res) {
+    // check to see if user logged in
+    res.render('index');
 })
 
-// Include auth controller
-app.use('/auth', require('./controller/auth')); 
+// include auth controller
+app.use('/auth', require('./controllers/auth'));
 
-
-
-// Initialize App on Port
-app.listen(process.env.PORT || 3000, (port) => {
-    console.log(`Working on ${port}`)
+// initialize App on Port
+app.listen(process.env.PORT || 3000, function() {
+    console.log(`Listening to the smooth sweet sounds of port ${process.env.PORT} in the morning ☕️.`);
 });
