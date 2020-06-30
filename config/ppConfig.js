98d45cfc17d865e08c8580bd1fccafdbef367cd9
Eryoung-610 <eryoung610@gmail.com>
2020-06-30 14:46:03 -0700

    Added middleware for sessions

--- config/ppConfig.js
+++ config/ppConfig.js
@@ -1,28 +1,33 @@
-const passport = require('passport')
+// import necessary libraries and modules
+const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const db = require ('../models')
+const db = require('../models');
 
-passport.sequelizeUser((user,cb) => {
+// serialize user
+passport.serializeUser(function(user, callback) {
     callback(null, user.id);
 })
 
-passport.deserializeUser((id,cb) => {
-    db.user.findByPk(id).then((user) => {
-        callback(null,user)
-    }).catch(callback)
+// deserialized version
+passport.deserializeUser(function(id, callback) {
+    db.user.findByPk(id).then(function(user) {
+        callback(null, user);
+    }).catch(callback);
 })
 
+// configure local variables/settings
 passport.use(new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password'
-}, function (email,password, callback) {
-    db.user.findOne({ where: {email}}).then(function(user) {
-        if(!user || !user.validPassword(password)) {
-            callback(null,false);
+}, function(email, password, callback) {
+    db.user.findOne({ where: { email }}).then(function(user) {
+        if (!user || !user.validPassword(password)) {
+            callback(null, false);
         } else {
-            callback(null,user);
+            callback(null, user);
         }
-    }).catch(callback)
+    }).catch(callback);
 }));
+
 
 module.exports = passport;
--- index.js
+++ index.js
@@ -6,16 +6,44 @@ const ejsLayouts = require("express-ejs-layouts");
 const helmet = require('helmet');
 const session = require("express-session");
 const flash = require("flash");
-
+const passport = require('./config/ppConfig')
+const db = require('./models')
 
 // app setup
 const app = Express();
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
 app.use(Express.urlencoded({ extended: false }));
 app.use(Express.static(__dirname + "/public"));
 app.set('view engine','ejs');
 app.use(ejsLayouts);
 app.use(require('morgan')('dev'));
 app.use(helmet());
+
+// Create new isntance of class sequelize store
+const sessionStore = new SequelizeStore({
+    db: db.sequelize,
+    expiration: 1000 * 60 * 30
+})
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: true
+}))
+
+sessionStore.sync();
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use((req,res,next) => {
+    res.locals.alert = req.flash()
+    res.locals.currentUser = req.user;
+
+    next();
+})
 
 // ROUTES
 app.get('/', function(req, res) {
